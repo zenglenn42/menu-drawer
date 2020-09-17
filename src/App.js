f4d913d0317ba76152aec59371639e9d83143a76
zenglenn42 <gsnospampls@pobox.com>
2020-09-17 14:32:42 -0500

    use className instead of style attribute for App-level css

--- src/App.js
+++ src/App.js
@@ -12,79 +12,63 @@ import {
   nestedLayoutReducer,
   singlePeerExpansionReducer
 } from './components/NestedAccordion/NestedAccordion'
-import {
-  appFrame,
-  header,
-  headerTitle,
-  headerSubTitle,
-  main,
-  article,
-  article2,
-  articleTitle,
-  accordion,
-  footer,
-  iconButton,
-  grow,
-  grow2x
-} from './style.js'
 import MenuDrawerApp from './components/MenuDrawerApp/MenuDrawerApp'
+import './App.css'
 
 function App() {
   return (
-    <>
-      <div style={appFrame}>
-        <header style={header}>
-          <p style={headerTitle}>Menu Drawer</p>
-          <p style={headerSubTitle}>Case Study</p>
-          <p style={headerSubTitle}>Transform an Accordion into a Menu Drawer</p>
-        </header>
-        <div style={grow} />
-        <main style={main}>
-          <article style={article}>
-            <header style={articleTitle}>Nested Accordion</header>
-            <div style={accordion}>
-              <Accordion
-                items={nestedAccordionData}
-                initialExpandedItems={[1]}
-                inputItemsReducer={nestedDataClosure(nestedAccordionDataOverrides)}
-                layoutReducer={nestedLayoutReducer}
-                expansionReducer={singlePeerExpansionReducer}
-              />
-            </div>
-          </article>
-          <article style={article2}>
-            <MenuDrawerApp 
-                items={nestedMenuData} 
-                title="Menu Drawer"
-                initialExpandedItems={[0, 1, 6]}
+    <div className="appFrame">
+      <header className="header" >
+        <p className="headerTitle">Menu Drawer</p>
+        <p className="headerSubTitle">Case Study</p>
+        <p className="headerSubTitle">Transform an Accordion into a Menu Drawer</p>
+      </header>
+      <div className="grow1x" />
+      <main className="main" >
+        <article className="article" >
+          <header className="articleTitle">Nested Accordion</header>
+          <div className="accordion" >
+            <Accordion
+              items={nestedAccordionData}
+              initialExpandedItems={[1]}
+              inputItemsReducer={nestedDataClosure(nestedAccordionDataOverrides)}
+              layoutReducer={nestedLayoutReducer}
+              expansionReducer={singlePeerExpansionReducer}
             />
-          </article>
-        </main>
-        <div style={grow2x} />
-        <footer style={footer}>
-          <span>Incremental Industries &copy; 2020</span>
-          <span style={{ flex: 1 }}></span>
-          <a
-            style={iconButton}
-            href="https://zenglenn42.github.io/portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="portfolio"
-          >
-            <PortfolioIcon />
-          </a>
-          <a
-            style={iconButton}
-            href="https://github.com/zenglenn42/menu-drawer/blob/master/README.md"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="github"
-          >
-            <GithubIcon />
-          </a>
-        </footer>
-      </div>
-    </>
+          </div>
+        </article>
+        <article className="article article2" >
+          <MenuDrawerApp 
+              items={nestedMenuData} 
+              title="Menu Drawer"
+              initialExpandedItems={[0, 1, 6]}
+          />
+        </article>
+      </main>
+      <div className="grow2x" />
+      <footer className="footer" >
+        <span>Incremental Industries &copy; 2020</span>
+        <span className="grow1x"></span>
+        <a
+          className="footerButton"
+          href="https://zenglenn42.github.io/portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="portfolio"
+        >
+          <PortfolioIcon />
+        </a>
+        <a
+          className="footerButton"
+          href="https://github.com/zenglenn42/menu-drawer/blob/master/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="github"
+        >
+          <GithubIcon />
+        </a>
+      </footer>
+    </div>
   )
 }
 
